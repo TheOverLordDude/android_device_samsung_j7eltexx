c70baf86edac5dfd2ccf72729d032614ef458173
TheOverLordDude <mldevelopingstudios@gmail.com>
2019-08-10 06:47:34 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@
 # Inherit from Exynos7580-common
 include device/samsung/exynos7580-common/BoardConfigCommon.mk
 
-TARGET_OTA_ASSERT_DEVICE := j7e3g,j7e3gxx,j7elte,j7eltexx
+TARGET_OTA_ASSERT_DEVICE := j700T,j7e3g,j7e3gxx,j7elte,j7eltexx
 
 DEVICE_PATH := device/samsung/j7eltexx
 
